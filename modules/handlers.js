3759988aaa192a36a2a3bbb53064591c28fccfae
Camilla Rees <camilla.c.rees@gmail.com>
2022-11-12 12:46:09 -0700

    connected with Mongo DBand added data to work with

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,41 +1,50 @@
 'use strict';
 
-// const Playlist = require('../models/playlist');
 const axios = require('axios');
+const Playlist = require('../model/playlist');
 const Handlers = {};
 
-Handlers.getKeyword = async (req, res, next) => {
-  const { searchQuery } = req.searchQuery.searchQuery;
-  const url = `https://api.spotify.com/v1/search?q=${searchQuery}&type=track&limit=6&market=ES`;
-  axios
-    .get(url)
-    .then(playlist => {
-      console.log(playlist.data);
-      const tracksArray = playlist.data.items.map(tracks => new Playlist(tracks));
-      res.status(200).send(tracksArray);
-    })
-    .catch(error => {
-      console.error(error);
-      next(error);
-    });
-};
+// Handlers.getKeyword = async (req, res, next) => {
+//   const { searchQuery } = req.searchQuery.searchQuery;
+//   const url = `https://api.spotify.com/v1/search?q=${searchQuery}&type=track&limit=6&market=ES`;
+//   axios
+//     .get(url)
+//     .then(playlist => {
+//       console.log(playlist.data);
+//       const tracksArray = playlist.data.items.map(tracks => new Playlist(tracks));
+//       res.status(200).send(tracksArray);
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       next(error);
+//     });
+// };
 
-Handlers.getGenre = async (req, res, next) => {
-  const { searchGenre } = req.query.searchGenre;
-  const url = `https://api.spotify.com/v1/search?genre=${searchGenre}&type=track&limit=6&market=ES`;
-  axios
-    .get(url)
-    .then(playlist => {
-      console.log(playlist.data);
-      const tracksArray = playlist.data.items.map(tracks => new Playlist(tracks));
-      res.status(200).send(tracksArray);
-    })
-    .catch(error => {
-      console.error(error);
-      next(error);
-    });
-};
+// Handlers.getGenre = async (req, res, next) => {
+//   const { searchGenre } = req.query.searchGenre;
+//   const url = `https://api.spotify.com/v1/search?genre=${searchGenre}&type=track&limit=6&market=ES`;
+//   axios
+//     .get(url)
+//     .then(playlist => {
+//       console.log(playlist.data);
+//       const tracksArray = playlist.data.items.map(tracks => new Playlist(tracks));
+//       res.status(200).send(tracksArray);
+//     })
+//     .catch(error => {
+//       console.error(error);
+//       next(error);
+//     });
+// };
 
+Handlers.getSavedPlaylist = async (req, res, next) => {
+  try {
+    const playlists = await Playlist.find({});
+    res.status(200).send(playlists);
+  } catch(error) {
+    console.error(error);
+    next(error);
+  }
+};
 // class Playlist {
 //   constructor(tracks) {
 //     this.items = tracks.
--- server.js
+++ server.js
@@ -3,47 +3,47 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const Handlers = require('./modules/handlers');
-const verifyUser = require('./auth.js');
+// const verifyUser = require('./auth.js');
 
 const app = express();
 app.use(cors());
-app.use(verifyUser);
+// app.use(verifyUser);
 
-app.use(express.json()); // has server update with json data 
+app.use(express.json()); // has server update with json data
 
 const PORT = process.env.PORT || 3002;
 
-// mongoose.connect(process.env.MONGO_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true});
-// const db = mongoose.connection;
+mongoose.connect(process.env.MONGO_DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
 
-// db.on('error', console.error.bind(console, 'connection error:'));
-// db.once('open', function() {
-//   console.log('Mongoose is connected');
-// });
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected');
+});
 
-app.get('/', (req, res) => res.send('test request received'));
+// app.get('/', (req, res) => res.send('test request received'));
 
-// Search keyword for tracks from user input
-app.get('/search', Handlers.getKeyword);
+// // Search keyword for tracks from user input
+// app.get('/search', Handlers.getKeyword);
 
-// Search for genre from user input 
-app.get('/search', Handlers.getGenre);
+// // Search for genre from user input 
+// app.get('/search', Handlers.getGenre);
 
-// Get playlist from Spotify
-app.get('/playlist/{playlist_id}', Handlers.getPlaylist);
+// // Get playlist from Spotify
+// app.get('/playlist/{playlist_id}', Handlers.getPlaylist);
 
-// Get saved playlist from database
+// // Get saved playlist from database
 app.get('/playlist', Handlers.getSavedPlaylist);
 
-// Create playlist
-app.post('/users/{user_id}/playlists', Handlers.createPlaylist);
+// // Create playlist
+// app.post('/users/{user_id}/playlists', Handlers.createPlaylist);
 
-// Delete playlist
-app.delete('/playlist/{playlist_id}/tracks', Handlers.deletePlaylist);
+// // Delete playlist
+// app.delete('/playlist/{playlist_id}/tracks', Handlers.deletePlaylist);
 
-// Annotate playlist
-app.put('/notes', Handlers.updateNote);
+// // Annotate playlist
+// app.put('/notes', Handlers.updateNote);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
